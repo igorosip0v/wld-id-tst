f768c802ea3613f0f7343eb358cf17f84a1900fb
Igor Osipov <igor.osipov@ottofeller.com>
2022-07-07 12:55:53 +0300

    rebuild

--- dist/cjs/widget/init.js
+++ dist/cjs/widget/init.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.init = void 0;
 var jsx_runtime_1 = require("react/jsx-runtime");
-var components_1 = require("components");
+var _components_1 = require("@components");
 var react_dom_1 = require("react-dom");
 var init = function (elementInput) {
     var mountNode = null;
@@ -13,7 +13,7 @@ var init = function (elementInput) {
         mountNode = document.getElementById(elementInput);
     }
     if (mountNode !== null) {
-        (0, react_dom_1.render)((0, jsx_runtime_1.jsx)(components_1.ReactWidget, {}), mountNode);
+        (0, react_dom_1.render)((0, jsx_runtime_1.jsx)(_components_1.ReactWidget, {}), mountNode);
     }
 };
 exports.init = init;
