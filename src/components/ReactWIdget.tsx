b68d592d3a859f9487aebeb3488f45bfe19eb083
Igor Osipov <igor.osipov@ottofeller.com>
2022-07-11 14:12:49 +0300

    add stitches

--- src/components/ReactWIdget.tsx
+++ src/components/ReactWIdget.tsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
+import { styled } from "@stitches/react";
+
+const Container = styled("div", {
+  backgroundColor: "Aquamarine",
+  fontSize: "20px",
+});
 
 export const ReactWidget = (): JSX.Element => {
-  const [name, _setName] = useState('Name')
-  return <div>{`I am ${name}`}</div>;
+  const [name, _setName] = useState("Name");
+  return <Container>{`I am ${name}`}</Container>;
 };
